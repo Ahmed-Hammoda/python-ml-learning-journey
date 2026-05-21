30425d59a136e06a9e65fde223fe4319fec9dab7
Ahmed Hamouda <hamoudaahmad8@example.com>
2026-05-21 20:44:49 +0100

    add the var lecture

--- python basics/variables.py
+++ python basics/variables.py
@@ -2,6 +2,32 @@
 """
 Created on Thu May 21 20:14:53 2026
 
-@author: hamou
+@author: ahmed hamouda
 """
+
+# in python you do not need the write the data type
+
+a = 5
+b = "ahmed"
+n = True
+
+# to know what is the data type
+
+type(a)
+
+# you can defind more than one var in the same line
+
+c,d,e,f = 6,7,8,9
+
+print(c)
+
+# 5,6,7,8,9,10,... is int data type
+
+# there is a float data type comes with bigger size
+k = 1.2
+print(type(k))
+
+# this is how to write the complex number
+
+g = 5 + 7j
 
