cf97a31d2a628980fe57d556d24989894db6c2c5
Ahmed Hamouda <hamoudaahmad8@example.com>
2026-05-25 22:57:18 +0100

    part from the string in python

--- python basics/05_String_in_python.py
+++ python basics/05_String_in_python.py
@@ -4,6 +4,8 @@ Created on Sat May 23 17:08:12 2026
 
 @author: ahmed hamouda
 """
+import numpy as np
+
 # in here we will learn how to deal with strings in python
 a = "hello i am ahmed"
 b = '0123456789' # '' or "" can create a string
@@ -39,4 +41,45 @@ print(a.title()) # make the first letter from each word capital
 print(d.upper()) # all letter are capital
 print(d.swapcase()) # swap each letter if its capital make it samll and the opposite
 print(d.center(30)) # make the string centered in 30 letter space 
+print(a.ljust(30)) # complete to 30 letter with spaces from the left
+print(a.rjust(30)) # complete to 30 letter with spaces from the right
+print(a.rjust(30 , "*")) # complete to 30 letter with a * (add the * in the right)
+print(b.zfill(20)) # complete to 20 letters with zeros (add them from the left)
+print(a.isalpha()) # are all the latters in this string an alphabet latters (true or false) "space is not an alphabet latter"
+print(a.strip()) # remove any space after the string
+print(a.rstrip()) # remove any space after the string (from right)
+print(a.lstrip()) # remove any space after the string (from left)
+# strip and just are canceled each other
+print("123", a ,"\n" ,a) # \n for a new line
+e = r"C:\windows\new folder"
+print(e) # we use the r before the string to ignore the \n and print it as it is
+f = "my name is \t\t\tahmed"
+print(f)
+g = '''hi
+hi
+hi
+hi
+i am ahmed hamouda'''
+print(g)
+print(a, end= '') #print a and b without any space between
+print(b)
+h = 'ahmed'
+i = "palestine"
+k = 23
+j = 'my name is %s and i am from %s and i am %05d years old' %(h,i,k) # %s for string and %d for numbers (%05d mean complete the number to 5 digits with 0)
+# if i did not put the 05 and keep it %5d python will complete the number with empty spaces
+print(j)
+print('I \'wnant\' food') # use \' to add the '' as string
+print(b.isdigit()) # is this string number or not
+print(a.isupper()) #is all the letter in this string uppercase or not
+print(a.islower()) #is all the letter in this string lowercase or not
+print(a.istitle()) #is this string work as a title or not
+print(a.startswith('hello')) # to check if the string start with any word you want
+print(a.endswith('ahmed')) # to check if the string end with any word you want
+l = '@@'.join(('ahmed','ali','sara')) # join the word inside the string with the @@ or any thing you want
+print(l)
+m = "the value of pi is {}".format(np.pi) # replace the {} with what is the format
+print(m)
+print("{0} and {1}".format('red', 'blue'))
+
 
