b266245bace38e7b339c2ba10b767bfef9b2e72e
Ahmed Hamouda <hamoudaahmad8@example.com>
2026-05-28 17:10:31 +0100

    files in python

---
+++ python basics/06_Handling_with_files.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+reated on Sat May 28 16:55:12 2026
+
+@author: ahmed hamouda
+"""
+f = open('C:\\Users\\hamou\\Desktop\\ME\\AI\\My-journey-in-Python\\python basics\\Files in python (for file 06)\\test.txt','w')
+f.write('write this line in the file')
+f.close()
+
